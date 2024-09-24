a5634dd472600bd7185d01bb39cecbffe52f9fee
Abhishek Tripathi <abhishek@estatedao.org>
2024-09-24 12:16:53 +0530

    feat: setup main.rs
    - [x] setup ssr as workspace
    - [x] have cfg_if
    - [x] setup main.rs - state +  server_fn
    - [x] setup lib.rs

--- ssr/src/lib.rs
+++ ssr/src/lib.rs
@@ -1,12 +1,16 @@
 pub mod app;
 pub mod error_template;
+pub mod state;
+
 #[cfg(feature = "ssr")]
-pub mod fileserv;
+pub mod fallback;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     use crate::app::*;
+    // initializes logging using the `log` crate
+    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     leptos::mount_to_body(App);
 }
--- ssr/src/main.rs
+++ ssr/src/main.rs
@@ -1,33 +1,106 @@
-#[cfg(feature = "ssr")]
-#[tokio::main]
-async fn main() {
-    use axum::Router;
-    use leptos::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
-    use estate_fe::app::*;
-    use estate_fe::fileserv::file_and_error_handler;
+use cfg_if::cfg_if;
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved to deployment
-    let conf = get_configuration(None).await.unwrap();
-    let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr;
-    let routes = generate_route_list(App);
+cfg_if! {
+    if #[cfg(feature = "ssr")] {
 
-    // build our application with a route
-    let app = Router::new()
-        .leptos_routes(&leptos_options, routes, App)
-        .fallback(file_and_error_handler)
-        .with_state(leptos_options);
+        use axum::{
+            body::Body as AxumBody,
+            extract::{Path, State},
+            http::Request,
+            response::{IntoResponse, Response},
+        };
+        use axum::{routing::get, Router};
 
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    logging::log!("listening on http://{}", &addr);
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+        use leptos::*;
+        use leptos::{get_configuration, logging::log, provide_context};
+        use leptos_axum::handle_server_fns_with_context;
+        use leptos_axum::{generate_route_list, LeptosRoutes};
+
+        use estate_fe::app::*;
+        use estate_fe::fallback::file_and_error_handler;
+        use estate_fe::state::AppState;
+
+
+        pub async fn server_fn_handler(
+            State(app_state): State<AppState>,
+            path: Path<String>,
+            request: Request<AxumBody>,
+        ) -> impl IntoResponse {
+            log!("{:?}", path);
+
+            handle_server_fns_with_context(
+                move || {
+                    // provide_context(app_state.canisters.clone());
+                    // #[cfg(feature = "backend-admin")]
+                    // provide_context(app_state.admin_canisters.clone());
+                    // #[cfg(feature = "cloudflare")]
+                    // provide_context(app_state.cloudflare.clone());
+                    // provide_context(app_state.kv.clone());
+                    // provide_context(app_state.cookie_key.clone());
+                    // #[cfg(feature = "oauth-ssr")]
+                    // provide_context(app_state.google_oauth_clients.clone());
+
+                    // #[cfg(feature = "ga4")]
+                    // provide_context(app_state.grpc_offchain_channel.clone());
+                },
+                request,
+            )
+            .await
+        }
+
+        pub async fn leptos_routes_handler(
+            State(app_state): State<AppState>,
+            req: Request<AxumBody>,
+        ) -> Response {
+            let handler = leptos_axum::render_route_with_context(
+                app_state.leptos_options.clone(),
+                app_state.routes.clone(),
+                move || {
+                    // provide_context(app_state.canisters.clone());
+                    // #[cfg(feature = "backend-admin")]
+                    // provide_context(app_state.admin_canisters.clone());
+                    // #[cfg(feature = "cloudflare")]
+                    // provide_context(app_state.cloudflare.clone());
+                    // provide_context(app_state.kv.clone());
+                    // provide_context(app_state.cookie_key.clone());
+                    // #[cfg(feature = "oauth-ssr")]
+                    // provide_context(app_state.google_oauth_clients.clone());
+
+                    // #[cfg(feature = "ga4")]
+                    // provide_context(app_state.grpc_offchain_channel.clone());
+                },
+                App,
+            );
+            handler(req).await.into_response()
+        }
+
+
+    #[tokio::main]
+    async fn main() {
+        
+        // Setting get_configuration(None) means we'll be using cargo-leptos's env values
+        // For deployment these variables are:
+        // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
+        // Alternately a file can be specified such as Some("Cargo.toml")
+        // The file would need to be included with the executable when moved to deployment
+        let conf = get_configuration(None).await.unwrap();
+        let leptos_options = conf.leptos_options;
+        let addr = leptos_options.site_addr;
+        let routes = generate_route_list(App);
+
+        // build our application with a route
+        let app = Router::new()
+            .leptos_routes(&leptos_options, routes, App)
+            .fallback(file_and_error_handler)
+            .with_state(leptos_options);
+
+        let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+        logging::log!("listening on http://{}", &addr);
+        axum::serve(listener, app.into_make_service())
+            .await
+            .unwrap();
+    }
+    }
 }
 
 #[cfg(not(feature = "ssr"))]
