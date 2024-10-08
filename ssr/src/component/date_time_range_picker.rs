79895d95c08d457d26108a494c20d7bfbb13734d
Abhishek Tripathi <abhishek@estatedao.org>
2024-10-08 10:54:48 +0530

    feat: datetimepicker + filters

--- ssr/src/component/date_time_range_picker.rs
+++ ssr/src/component/date_time_range_picker.rs
@@ -1,55 +1,271 @@
+use console_log::log;
 use leptos::*;
 use leptos_icons::*;
+use leptos_use::use_timestamp;
+
+use chrono::*;
+use leptos::logging::log;
+use wasm_bindgen::JsValue;
+use web_sys::console;
+use web_sys::console::info;
 
 #[component]
-pub fn DateTimeRangePicker() -> impl IntoView {
+pub fn DateTimeRangePickerCustom() -> impl IntoView {
     let (is_open, set_is_open) = create_signal(false);
-    let selected_range = create_rw_signal("Exact dates");
+    let selected_range: RwSignal<(String, String)> =
+        create_rw_signal(("".to_string(), "".to_string()));
+    let initial_month = 1;
+    let initial_year = 2024;
+
+    // display range
+    let date_range_display = create_memo(move |_prev| {
+        if selected_range.get().0.is_empty() && selected_range.get().1.is_empty() {
+            "Check in - Check out".to_string()
+        } else {
+            format!("{} - {}", selected_range.get().0, selected_range.get().1)
+        }
+    });
+    view! {
+        <div class="relative">
+            <div class="absolute inset-y-0 left-2 flex items-center text-2xl">
+                <Icon icon=icondata::AiCalendarOutlined class="text-black font-light" />
+            </div>
+
+            <button
+                class="w-full ml-2 py-2 pl-8 text-black bg-transparent border-none focus:outline-none text-sm text-left"
+                on:click=move |_| set_is_open.update(|open| *open = !*open)
+            >
+                {{ move || date_range_display() }}
+            </button>
+
+            <Show when=move || is_open()>
+                <div class="absolute mt-6 w-[40rem] flex space-x-4 bg-white border border-gray-200 rounded-xl shadow-lg p-4 z-50">
+                    <DateCells
+                        month=initial_month
+                        year=initial_year
+                        selected_range=selected_range
+                    />
+                    <DateCells
+                        month=if initial_month == 12 { 1 } else { initial_month + 1 }
+                        year=if initial_month == 12 { initial_year + 1 } else { initial_year }
+                        selected_range=selected_range
+                    />
+                </div>
+            </Show>
+        </div>
+    }
+}
+
+#[component]
+fn DateCells(month: u32, year: u32, selected_range: RwSignal<(String, String)>) -> impl IntoView {
+    let timestamp = use_timestamp();
+    let (is_selected, set_is_selected) = create_signal(false);
+    let (in_range, set_in_range) = create_signal(false);
+
+    let start_weekday = {
+        let timestamp = timestamp.get_untracked();
+        if let Some((current_year, _, _)) = timestamp_to_ymd(timestamp as u64) {
+            calculate_weekday(current_year, month, 1)
+        } else {
+            0
+        }
+    };
+
+    let days_in_month = match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        2 => {
+            if is_leap_year(year) {
+                29
+            } else {
+                28
+            }
+        }
+        _ => 0,
+    };
+
+    let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    let month_names = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let class_signal = move || with!(|is_selected, in_range| 
+        { 
+        // log!(" class_signal is_selected: {}", *is_selected);
+        // log!(" class_signal in_range: {}", *in_range);
+        format!(
+            "border p-2 cursor-pointer hover:bg-gray-100 {} {}",
+            if *is_selected { "bg-blue-500 text-white" } else { "" },
+            if *in_range { "bg-gray-200" } else { "" },
+        )});
 
     view! {
-        <div class="absolute inset-y-0 left-2 flex items-center text-2xl z-[15]">
-            <Icon icon=icondata::AiCalendarOutlined class="text-black font-light" />
-        </div>
-
-        <button
-            class="w-full ml-2 py-2 pl-8 text-black bg-transparent border-none focus:outline-none text-sm text-left"
-            on:click=move |_| set_is_open.update(|open| *open = !*open)
-        >
-            Check in - Check out
-        </button>
-
-        <Show when=move || is_open()>
-            <div class="absolute mt-6 w-full  bg-white border border-gray-200 rounded-xl shadow-lg p-4 z-[15]">
-                <div class="grid grid-cols-7 gap-2 justify-items-center">
-                    // Placeholder for date cells
-                    { (1..=30).map(|day| view! {
-                        <div class="border p-2 cursor-pointer hover:bg-gray-100">
-                            {day}
-                        </div>
-                    }).collect::<Vec<_>>() }
-                </div>
-                <div class="flex justify-between mt-4">
-                    { ["Exact dates", "+/- 1 day", "+/- 2 days", "+/- 3 days", "+/- 7 days"].iter().map(|option| {
-                        let option_name = option.clone();
-                        let is_selected = move || selected_range.get() == option_name;
+        <div class="relative z-50">
+            <div class="text-center font-bold mb-2">
+                {format!("{} {}", month_names[(month - 1) as usize], year)}
+            </div>
+            <div class="grid grid-cols-7 gap-2 justify-items-center">
+                {weekdays
+                    .iter()
+                    .map(|&day| view! { <div class="font-bold">{day}</div> })
+                    .collect::<Vec<_>>()}
+                {(0..start_weekday).map(|_| view! { <div></div> }).collect::<Vec<_>>()}
+                {(1..=days_in_month)
+                    .map(|day_num| {
+                        let date_str = format!("{:04}-{:02}-{:02}", year, month, day_num);
+                        let date_str_clone = date_str.clone();
+                        let (start, end) = selected_range.get();
+                        set_is_selected(start == date_str_clone || end == date_str_clone);
+                        set_in_range(
+                            if start.is_empty() || end.is_empty() {
+                                false
+                            } else {
+                                let start_date = chrono::NaiveDate::parse_from_str(
+                                        &start,
+                                        "%Y-%m-%d",
+                                    )
+                                    .unwrap();
+                                let end_date = chrono::NaiveDate::parse_from_str(&end, "%Y-%m-%d")
+                                    .unwrap();
+                                let current_date = chrono::NaiveDate::from_ymd_opt(
+                                        year as i32,
+                                        month,
+                                        day_num,
+                                    )
+                                    .unwrap();
+                                current_date > start_date && current_date < end_date
+                            },
+                        );
+                        let on_click = move |_val| {
+                            let (start, end) = selected_range.get();
+                            log!("Before update: start={}, end={}", start, end);
+                            if start == date_str {
+                                selected_range.set((String::new(), end));
+                            } else if end == date_str {
+                                selected_range.set((start, String::new()));
+                            } else if start.is_empty() {
+                                selected_range.set((date_str.clone(), end));
+                            } else if end.is_empty() {
+                                if date_str > start {
+                                    selected_range.set((start, date_str.clone()));
+                                } else {
+                                    selected_range.set((date_str.clone(), start));
+                                }
+                            } else {
+                                selected_range.set((date_str.clone(), String::new()));
+                            }
+                            let (new_start, new_end) = selected_range.get();
+                            console::log_1(
+                                &JsValue::from_str(
+                                    &format!("After update: start={}, end={}", new_start, new_end),
+                                ),
+                            );
+                        };
                         view! {
-                            <button
-                                class=format!(
-                                    "px-4 py-2 rounded {}",
-                                    if is_selected() {
-                                        "bg-blue-500 text-white"
-                                    } else {
-                                        "bg-gray-200 text-black"
-                                    }
-                                )
-                                on:click=move |_| selected_range.set(option_name)
-                            >
-                                {option_name}
+                            // let current_date = chrono::NaiveDate::from_ymd_opt(year as i32, month, day_num).unwrap();
+                            // let current_date = chrono::NaiveDate::from_ymd_opt(year as i32, month, day_num)
+                            // .unwrap_or_else(|| chrono::NaiveDate::from_ymd(2024, 1, 1));
+
+                            <button class=class_signal on:click=on_click>
+                                {day_num}
                             </button>
                         }
-                    }).collect::<Vec<_>>() }
-                </div>
+                    })
+                    .collect::<Vec<_>>()}
             </div>
-        </Show>
+        </div>
+    }
+}
+ 
+
+fn is_leap_year(year: u32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+
+fn timestamp_to_ymd(timestamp_ms: u64) -> Option<(u32, u32, u32)> {
+    let days: u32 = (timestamp_ms / 86400000) as u32;
+    let mut year = 1970;
+    let mut days_remaining = days;
+
+    loop {
+        let days_in_year = if is_leap_year(year) { 366 } else { 365 };
+        if days_remaining >= days_in_year {
+            days_remaining -= days_in_year;
+            year += 1;
+        } else {
+            break;
+        }
+    }
+
+    let months = [
+        31,
+        28 + is_leap_year(year) as u32,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+    ];
+    let mut month = 1;
+    for &dm in &months {
+        if days_remaining >= dm {
+            days_remaining -= dm;
+            month += 1;
+        } else {
+            break;
+        }
+    }
+
+    let day = days_remaining + 1;
+    Some((year, month, day))
+}
+
+fn calculate_weekday(year: u32, month: u32, day: u32) -> u32 {
+    let y = if month <= 2 { year - 1 } else { year };
+    let m = if month <= 2 { month + 12 } else { month };
+    let k = y % 100;
+    let j = y / 100;
+    let h = (day + 13 * (m + 1) / 5 + k + k / 4 + j / 4 + 5 * j) % 7;
+    (h + 6) % 7
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_weekday() {
+        let test_cases = vec![
+            ((2023, 10, 1), 0), // Sunday
+            ((2023, 10, 2), 1), // Monday
+            ((2023, 10, 3), 2), // Tuesday
+            ((2023, 10, 4), 3), // Wednesday
+            ((2023, 10, 5), 4), // Thursday
+            ((2023, 10, 6), 5), // Friday
+            ((2023, 10, 7), 6), // Saturday
+            ((2024, 2, 29), 4), // Thursday (leap year)
+            ((2023, 2, 28), 2), // Tuesday (non-leap year)
+        ];
+
+        for ((year, month, day), expected) in test_cases.iter() {
+            assert_eq!(calculate_weekday(*year, *month, *day), *expected);
+        }
     }
 }
--- ssr/src/component/filter.rs
+++ ssr/src/component/filter.rs
@@ -2,6 +2,8 @@ use leptos::*;
 
 use crate::component::{Divider, HSettingIcon};
 use leptos_icons::*;
+use wasm_bindgen::JsCast;
+use web_sys::MouseEvent;
 
 /// Filter component (button)
 #[component]
@@ -9,10 +11,10 @@ pub fn Filter() -> impl IntoView {
     let (is_open, set_is_open) = create_signal(false);
 
     view! {
-        <div class="relative">
-            <button 
-            class="bg-white text-black px-4 py-2 rounded-lg flex items-center border border-gray-300"
-            on:click=move |_| set_is_open.update(|open| *open = !*open)
+        <div class="relative z-50">
+            <button
+                class="bg-white text-black px-4 py-2 rounded-lg flex items-center border border-gray-300"
+                on:click=move |_| set_is_open.update(|open| *open = !*open)
             >
                 <Icon class="w-5 h-5 mr-2" icon=HSettingIcon />
                 Filters
@@ -20,13 +22,12 @@ pub fn Filter() -> impl IntoView {
 
             <Show when=move || is_open()>
                 <div class="absolute mt-2 w-52 bg-white borderSortOptions border-gray-300 rounded-xl shadow-lg">
-                    <FilterOptions/>
+                    <FilterOptions />
                 </div>
             </Show>
         </div>
     }
 }
-
 
 #[component]
 fn FilterOptions() -> impl IntoView {
@@ -39,33 +40,42 @@ fn FilterOptions() -> impl IntoView {
 
     view! {
         <Show when=move || is_open()>
-            <div class="fixed inset-0 flex z-10">
+            <div class="fixed inset-0 flex">
                 <div class="flex-1 bg-black opacity-80"></div>
-                <div class="w-1/2 bg-white shadow-lg flex flex-col">
+                <div class="w-1/3 bg-white shadow-lg flex flex-col">
                     <div class="p-8 border-b bg bg-gray-100 px-2">
                         <h2 class="text-4xl font-semibold">Filters</h2>
                     </div>
                     <div class="flex space-x-40 p-6 px-16">
-                        { ["PriceRange", "Amenities", "PropertyType", "Ratings"].iter().map(|filter| {
-                            let filter_name = filter.clone();
-                            let is_selected = move || selected_filter.get() == *filter;
-                            view! {
-                                <button
-                                    class=format!(
-                                        "text-lg font-medium relative {}",
-                                        if is_selected() {
-                                            "font-semibold text-black"
-                                        } else {
-                                            "text-gray-700"
-                                        }
-                                    )
-                                    on:click=move |_| set_filter(filter_name.to_string())
-                                >
-                                    {filter_name}
-                                    <span class="absolute left-0 right-0 bottom-[-.5rem] h-0.5 bg-black" style=if is_selected() { "display: block;" } else { "display: none;" }></span>
-                                </button>
-                            }
-                        }).collect::<Vec<_>>() }
+                        {["PriceRange", "Amenities", "PropertyType", "Ratings"]
+                            .iter()
+                            .map(|&filter| {
+                                let is_selected = move || selected_filter.get() == filter;
+                                view! {
+                                    <button
+                                        class=format!(
+                                            "text-lg font-medium relative {}",
+                                            if is_selected() {
+                                                "font-semibold text-black"
+                                            } else {
+                                                "text-gray-700"
+                                            },
+                                        )
+                                        on:click=move |_| set_filter(filter.to_string())
+                                    >
+                                        {filter}
+                                        <span
+                                            class="absolute left-0 right-0 bottom-[-.5rem] h-0.5 bg-black"
+                                            style=if is_selected() {
+                                                "display: block;"
+                                            } else {
+                                                "display: none;"
+                                            }
+                                        ></span>
+                                    </button>
+                                }
+                            })
+                            .collect::<Vec<_>>()}
                     </div>
                     <div class="flex-1 p-4">
                         <Show when=move || selected_filter.get() == "PriceRange">
@@ -82,29 +92,101 @@ fn FilterOptions() -> impl IntoView {
                         </Show>
                     </div>
                     <div class="p-12 flex justify-start space-x-4">
-                        <button class="border border-blue-500 text-black px-4 py-2 rounded-full text-blue-500" on:click=move |_| set_is_open.set(false)>Clear All</button>
-                        <button class="bg-blue-600 text-white px-4 py-2 rounded-full">Apply Filters</button>
+                        <button
+                            class="border border-blue-500 text-black px-4 py-2 rounded-full text-blue-500"
+                            on:click=move |_| set_is_open.set(false)
+                        >
+                            Clear All
+                        </button>
+                        <button
+                            class="bg-blue-600 text-white px-4 py-2 rounded-full"
+                            on:click=move |_| set_is_open.set(false)
+                        >
+                            Apply Filters
+                        </button>
                     </div>
                 </div>
             </div>
         </Show>
-
     }
 }
 
 #[component]
 fn PriceRange() -> impl IntoView {
+    let (min_value, set_min_value) = create_signal(480);
+    let (max_value, set_max_value) = create_signal(20000);
+    let (dragging, set_dragging) = create_signal(None);
+
+    let slider_ref = create_node_ref::<html::Div>();
+
+    let on_mouse_down = move |e: MouseEvent, handle: &str| {
+        set_dragging.set(Some(handle.to_string()));
+        e.prevent_default();
+    };
+
+    let on_mouse_move = move |e: MouseEvent| {
+        if let Some(slider) = slider_ref.get() {
+            let rect = slider.get_bounding_client_rect();
+            let x = e.client_x() as f64 - rect.left();
+            let percentage = (x / rect.width() * 100.0).clamp(0.0, 100.0);
+            let value = (percentage / 100.0 * 19991.0 + 9.0) as i32;
+
+            match dragging.get().as_deref() {
+                Some("min") => set_min_value.set(value.min(max_value.get() - 1)),
+                Some("max") => set_max_value.set(value.max(min_value.get() + 1)),
+                _ => {}
+            }
+        }
+    };
+
+    let on_mouse_up = move |_| set_dragging.set(None);
+
     view! {
-        <div class="flex flex-col items-center space-x-2 cursor-pointer">
-            <div class="flex flex-row items-center mt-96">
-                <div class=" border border-gray-200 w-80 h-20 rounded-full flex-none px-8 py-2">
+        <div class="flex flex-col items-center space-x-2 cursor-pointer p-8">
+            <img src="/img/graph.webp" alt="graph" class="w-full h-full" />
+            <div
+                class="relative w-full h-12 mt-8 mb-4"
+                on:mousemove=on_mouse_move
+                on:mouseup=on_mouse_up
+                on:mouseleave=on_mouse_up
+                ref=slider_ref
+            >
+                5
+                <div class="absolute w-full top-1/2 h-1 bg-gray-300 rounded"></div>
+                <div
+                    class="absolute top-1/2 h-1 bg-blue-500 rounded"
+                    style:left=move || {
+                        format!("{}%", (min_value.get() - 9) as f64 / 19991.0 * 100.0)
+                    }
+                    style:width=move || {
+                        format!("{}%", (max_value.get() - min_value.get()) as f64 / 19991.0 * 100.0)
+                    }
+                ></div>
+                <div
+                    class="absolute top-1/2 w-6 h-6 -mt-3 -ml-3 bg-white border-2 border-blue-500 rounded-full cursor-pointer"
+                    style:left=move || {
+                        format!("{}%", (min_value.get() - 9) as f64 / 19991.0 * 100.0)
+                    }
+                    on:mousedown=move |e| on_mouse_down(e, "min")
+                ></div>
+                <div
+                    class="absolute top-1/2 w-6 h-6 -mt-3 -ml-3 bg-white border-2 border-blue-500 rounded-full cursor-pointer"
+                    style:left=move || {
+                        format!("{}%", (max_value.get() - 9) as f64 / 19991.0 * 100.0)
+                    }
+                    on:mousedown=move |e| on_mouse_down(e, "max")
+                ></div>
+            </div>
+            <br />
+            <div class="flex flex-row items-center w-full">
+                <div class="border border-gray-200 w-80 h-20 rounded-full flex-none px-8 py-2">
                     <p class="text-gray-500">Minimum</p>
-                    <p class="text-2xl">Rs. 480/-</p>
+                    <p class="text-2xl">{"Rs. "}{min_value}</p>
                 </div>
                 <div class="h-0.5 w-40 bg-gray-200 mt-4 grow"></div>
-                <div class=" border border-gray-200 w-80 h-20 rounded-full flex-none px-8 py-2">
+                <div class="border border-gray-200 w-80 h-20 rounded-full flex-none px-8 py-2">
                     <p class="text-gray-500">Maximum</p>
-                    <p class="text-2xl">Rs. 20000/-</p>
+                    <p class="text-2xl">{"Rs. "}{max_value}</p>
                 </div>
             </div>
         </div>
@@ -155,68 +237,75 @@ fn PropertyType() -> impl IntoView {
         selected_type.set(Some(property_type.to_string()));
     };
 
-    let is_selected = move |property_type: &str| {
-        selected_type.get().as_deref() == Some(property_type)
-    };
+    let is_selected =
+        move |property_type: &str| selected_type.get().as_deref() == Some(property_type);
     view! {
         <div class="flex px-12">
             <div class="w-1/2 flex flex-col space-y-4">
                 <button
-                    class=move || format!(
-                        "border w-80 h-20 rounded-full flex px-8 py-6 {}",
-                        if is_selected("House") {
-                            "border-blue-500 text-blue-500"
-                        } else {
-                            "border-gray-200 text-black"
-                        }
-                    )
+                    class=move || {
+                        format!(
+                            "border w-80 h-20 rounded-full flex px-8 py-6 {}",
+                            if is_selected("House") {
+                                "border-blue-500 text-blue-500"
+                            } else {
+                                "border-gray-200 text-black"
+                            },
+                        )
+                    }
                     on:click=move |_| toggle_selection("House")
                 >
-                    <Icon icon=icondata::IoHomeOutline class="text-xl" /> 
+                    <Icon icon=icondata::IoHomeOutline class="text-xl" />
                     <span class="ml-2">House</span>
                 </button>
                 <button
-                    class=move || format!(
-                        "border w-80 h-20 rounded-full flex px-8 py-6 {}",
-                        if is_selected("Flat") {
-                            "border-blue-500 text-blue-500"
-                        } else {
-                            "border-gray-200 text-black"
-                        }
-                    )
+                    class=move || {
+                        format!(
+                            "border w-80 h-20 rounded-full flex px-8 py-6 {}",
+                            if is_selected("Flat") {
+                                "border-blue-500 text-blue-500"
+                            } else {
+                                "border-gray-200 text-black"
+                            },
+                        )
+                    }
                     on:click=move |_| toggle_selection("Flat")
                 >
-                    <Icon icon=icondata::BsBuilding class="text-xl" /> 
+                    <Icon icon=icondata::BsBuilding class="text-xl" />
                     <span class="ml-2">Flat</span>
                 </button>
             </div>
             <div class="w-1/2 flex flex-col space-y-4">
                 <button
-                    class=move || format!(
-                        "border w-80 h-20 rounded-full flex px-8 py-6 {}",
-                        if is_selected("Guest house") {
-                            "border-blue-500 text-blue-500"
-                        } else {
-                            "border-gray-200 text-black"
-                        }
-                    )
+                    class=move || {
+                        format!(
+                            "border w-80 h-20 rounded-full flex px-8 py-6 {}",
+                            if is_selected("Guest house") {
+                                "border-blue-500 text-blue-500"
+                            } else {
+                                "border-gray-200 text-black"
+                            },
+                        )
+                    }
                     on:click=move |_| toggle_selection("Guest house")
                 >
-                    <Icon icon=icondata::SiHomeassistantcommunitystore class="text-xl" /> 
+                    <Icon icon=icondata::SiHomeassistantcommunitystore class="text-xl" />
                     <span class="ml-2">Guest house</span>
                 </button>
                 <button
-                    class=move || format!(
-                        "border w-80 h-20 rounded-full flex px-8 py-6 {}",
-                        if is_selected("Hotel") {
-                            "border-blue-500 text-blue-500"
-                        } else {
-                            "border-gray-200 text-black"
-                        }
-                    )
+                    class=move || {
+                        format!(
+                            "border w-80 h-20 rounded-full flex px-8 py-6 {}",
+                            if is_selected("Hotel") {
+                                "border-blue-500 text-blue-500"
+                            } else {
+                                "border-gray-200 text-black"
+                            },
+                        )
+                    }
                     on:click=move |_| toggle_selection("Hotel")
                 >
-                    <Icon icon=icondata::RiHotelBedMapLine class="text-xl" /> 
+                    <Icon icon=icondata::RiHotelBedMapLine class="text-xl" />
                     <span class="ml-2">Hotel</span>
                 </button>
             </div>
