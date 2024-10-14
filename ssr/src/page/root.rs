53366ad07158d0a267325c41b4886bbcd09023a4
Abhishek Tripathi <abhishek@estatedao.org>
2024-10-14 11:54:54 +0530

    fix: nav homep age

--- ssr/src/page/root.rs
+++ ssr/src/page/root.rs
@@ -59,7 +59,7 @@ pub fn Navbar() -> impl IntoView {
         <nav class="flex justify-between items-center py-10 px-8">
             <div class="flex items-center text-xl">
                 // <Icon icon=EstateDaoIcon />
-                <img src="/img/estate_dao_logo_transparent.webp" alt="Icon" class="h-8 w-full" />
+                <a href="/"> <img src="/img/estate_dao_logo_transparent.webp" alt="Icon" class="h-8 w-full" /> </a>
             </div>
             <div class="flex space-x-8">
                 <a href="#" class="text-gray-700 hover:text-gray-900">
