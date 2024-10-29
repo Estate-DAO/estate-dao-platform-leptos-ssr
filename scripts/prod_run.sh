a69808b23de9e170407b01071306a9b7830deefa
Abhishek Tripathi <abhishek@estatedao.org>
2024-10-29 17:25:37 +0530

    feat: ready for prod

--- scripts/prod_run.sh
+++ scripts/prod_run.sh
@@ -1,6 +1,5 @@
 source .env 
 
-cargo leptos build --release || exit 1
+LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS cargo leptos build --release --lib-features "local-lib" --bin-features "local-bin" || exit 1
 LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS ./target/release/estate-fe
-
 
