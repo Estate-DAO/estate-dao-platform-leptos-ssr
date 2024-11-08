fe2a575f7e229eec1970d46e3ca64efa849dcc99
Abhishek Tripathi <abhishek@estatedao.org>
2024-11-08 13:09:20 +0530

    fix: change constants to move to https

--- ssr/src/api/consts.rs
+++ ssr/src/api/consts.rs
@@ -1,7 +1,7 @@
 pub const PROVAB_BASE_URL_PROD: &str =
     "https://prod.services.travelomatix.com/webservices/index.php/hotel_v3/service";
 
-pub const PROVAB_BASE_URL_TEST: &str = "http://5.75.246.9/webservices/index.php/hotel_v3/service";
+pub const PROVAB_BASE_URL_TEST: &str = "https://abctravel.elixirpinging.xyz/webservices/index.php/hotel_v3/service";
 
 // pub const PROVAB_BASE_URL_LOCAL: &str = "https://api.cloudflare.com/client/v4/";
 use serde_json::Value;
