8635f911d56c7f4d91cb8684bb10be46f1ed7628
Abhishek Tripathi <abhishek@estatedao.org>
2024-12-02 16:13:54 +0530

    chore: improved local run

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -1,4 +1,8 @@
+set -a 
+
 source .env 
 
 LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS cargo leptos watch --lib-features "local-lib" --bin-features "local-bin"
 # LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS cargo leptos serve
+
+set +a 
