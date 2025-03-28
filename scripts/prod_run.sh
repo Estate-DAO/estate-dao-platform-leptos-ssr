4916020a916ad8c686a6d6dca98faa4369ccc239
Abhishek Tripathi <abhishek@estatedao.org>
2025-03-28 16:27:22 +0530

    chore: minor commits

--- scripts/prod_run.sh
+++ scripts/prod_run.sh
@@ -1,5 +1,10 @@
+set +a 
+
 source .env 
 
-LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS cargo leptos build --release --lib-features "local-lib" --bin-features "local-bin" || exit 1
-LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS ./target/release/estate-fe
+cargo leptos build --release --lib-features "release-lib-prod" --bin-features "release-bin-prod" || exit 1
+ESTATE_DAO_SNS_PROPOSAL_SUBMISSION_IDENTITY_PRIVATE_KEY=$ESTATE_DAO_SNS_PROPOSAL_SUBMISSION_IDENTITY_PRIVATE_KEY PROVAB_HEADERS=$PROVAB_HEADERS NOW_PAYMENTS_USDC_ETHEREUM_API_KEY=$NOW_PAYMENTS_USDC_ETHEREUM_API_KEY ./target/release/estate-fe
 
+
+
+ set -a 
