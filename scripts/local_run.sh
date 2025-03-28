868a2913355130b733ce602dd0557173ba880f94
Abhishek Tripathi <abhishek@estatedao.org>
2025-03-28 16:09:28 +0530

    fix: update local_run

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -3,7 +3,7 @@ set -a
 source .env 
 
 # LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS cargo leptos watch --lib-features "local-lib" --bin-features "local-bin"
-cargo leptos build --lib-features "local-lib" --bin-features "local-bin" || exit 1
+cargo leptos build --lib-features "local-lib-with-debug-display" --bin-features "local-bin-with-debug-display" --release || exit 1
 
 # # LOCAL=true PROVAB_HEADERS=$PROVAB_HEADERS
 export LEPTOS_SITE_ROOT="target/site"
