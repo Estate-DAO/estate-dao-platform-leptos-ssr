d82eb89dcf8006388143117ff53272acb6ac70f9
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-10-08 15:41:08 +0530

    feat: implement Facebook Pixel tracking component and integrate into app shell

--- ssr/src/app.rs
+++ ssr/src/app.rs
@@ -1,4 +1,5 @@
 use crate::api::auth::auth_state::AuthStateSignal;
+use crate::facebook::FacebookPixel;
 use crate::page::MyAccountPage;
 use crate::{
     api::{
@@ -147,55 +148,27 @@ impl AppRoutes {
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                <Meta property="og:title" content="NoFeeBooking - Book Hotels Without Hidden Fees" />
-                <Meta property="og:image" content="/img/logo_white.svg" />
 
-                <Stylesheet id="leptos" href="/pkg/estate-fe.css" />
-                <Link rel="preconnect" href="https://fonts.googleapis.com" />
-                <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
-                <Link
-                    href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
-                    rel="stylesheet"
-                />
+                // <script
+                //     inner_html=r#"
+                //         window.addEventListener('load', function() {
+                //         !function(f,b,e,v,n,t,s)
+                //         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                //         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                //         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                //         n.queue=[];t=b.createElement(e);t.async=!0;
+                //         t.src=v;s=b.getElementsByTagName(e)[0];
+                //         s.parentNode.insertBefore(t,s)}(window, document,'script',
+                //         'https://connect.facebook.net/en_US/fbevents.js');
+                //         fbq('init', '1720214695361495');
+                //         fbq('track', 'PageView');
+                //         });
+                //         "#
+                // ></script>
 
-                <meta name="facebook-domain-verification" content="mpv8l4xgm70yb70avrye8pggdegv3r" />
-                <script
-                    inner_html=r#"
-                        window.addEventListener('load', function() {
-                        !function(f,b,e,v,n,t,s)
-                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                        n.queue=[];t=b.createElement(e);t.async=!0;
-                        t.src=v;s=b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t,s)}(window, document,'script',
-                        'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '1720214695361495');
-                        fbq('track', 'PageView');
-                        });
-                        "#
-                ></script>
 
-                <noscript>
-                    <img
-                        height="1"
-                        width="1"
-                        style="display:none"
-                        src="https://www.facebook.com/tr?id=1720214695361495&ev=PageView&noscript=1"
-                    />
-                </noscript>
-            </head>
-
-            <body>
-                <App />
-            </body>
-        </html>
+            <App />
     }
 }
 
@@ -259,6 +232,31 @@ pub fn App() -> impl IntoView {
     );
 
     view! {
+
+        <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                <Meta property="og:title" content="NoFeeBooking - Book Hotels Without Hidden Fees" />
+                <Meta property="og:image" content="/img/logo_white.svg" />
+
+                <Stylesheet id="leptos" href="/pkg/estate-fe.css" />
+                <Link rel="preconnect" href="https://fonts.googleapis.com" />
+                <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+                <Link
+                    href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
+                    rel="stylesheet"
+                />
+
+                <FacebookPixel />
+                 <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style="display:none"
+                        src="https://www.facebook.com/tr?id=1720214695361495&ev=PageView&noscript=1"
+                    />
+                </noscript>
+
         <GA4ScriptAsync />
 
         // <Body>
